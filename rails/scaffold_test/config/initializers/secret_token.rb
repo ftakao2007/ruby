d33f881ec7b56d61fa2f945a2332903d899dc70f
Takao Fukui <fukui-takao@jsdnet.co.jp>
2015-11-25 13:03:41 +0900

    test

--- rails/scaffold_test/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ScaffoldtestFtakao2007::Application.config.secret_token = '2a29018b729eab2f7403ad4a65b8720db315bceb5850a8a9b3825e7c4c81d3dce50d6cfd2c5a4da860244bd1580f91a79fe830f74663a9655ca6d40bc9b85325'
