6b74d028946e7df23cdcae2eef009a10284fea2c
ftakao2007 <ftakao2007@gmail.com>
2018-01-02 03:16:37 +0900

    update

--- rails/heroku_test/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-BooksFtakao2007::Application.config.session_store :cookie_store, key: '_books-ftakao2007_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# BooksFtakao2007::Application.config.session_store :active_record_store
